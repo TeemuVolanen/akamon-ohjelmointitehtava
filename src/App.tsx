2042683b335aaeabdfd2ef96b81332ad1b5554f1
TeemuVolanen <teemu.k.volanen@student.jyu.fi>
2023-03-06 18:18:56 +0200

    Ensimmäinen versio jossa json serveriltä haetaan data ja laitetaan sivustolle näkyville

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useState, useEffect} from 'react'
+//import React from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
+interface dataObject {
+  timestamp: string,
+  price: number,
+  deliveryArea: string,
+  unit: string  
+}
+
 function App() {
+
+  const [data, setData] = useState<dataObject[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:3001/spot-data')
+    .then(response => response.json())
+    .then(res => setData(res as dataObject[]))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Akamon - spothinta ohjelmointitehtävä</h1>
+        {data.map(d =>
+          <p key={d.timestamp}>{d.price}</p>
+        )}
     </div>
   );
 }
